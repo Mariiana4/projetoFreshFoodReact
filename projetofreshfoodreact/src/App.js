8d94b0aea40d0a667020649b9c85143ba1ccf06e
Mariana Gonçalves <mariana.goncalves@maristavirtual.org.br>
2023-02-25 15:50:07 -0300

    Cabecalho:
    - parte 01: nav e logo OK
    - parte 02: campo de pesquisa e container com imagem OK
    
    - Pendente: funcionalidade da barra de pesquisa

--- projetofreshfoodreact/src/App.js
+++ projetofreshfoodreact/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import CabecalhoParte01 from './componentes/cabecalho/parte01/CabecalhoParte01';
+import CabecalhoParte02 from './componentes/cabecalho/parte02/CabecalhoParte02';
 
 function App() {
   return (
     <div className="App">
-      
+      <CabecalhoParte01/>
+      <CabecalhoParte02/>
     </div>
   );
 }
