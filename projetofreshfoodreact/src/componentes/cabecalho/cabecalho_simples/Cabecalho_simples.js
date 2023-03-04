175c3586dad93e264462d38812dca98899e8a883
Mariana Gonçalves <mariana.goncalves@maristavirtual.org.br>
2023-03-04 16:36:03 -0300

    Cabecalho simples feito

---
+++ projetofreshfoodreact/src/componentes/cabecalho/cabecalho_simples/Cabecalho_simples.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Cabecalho_simples.css'
+
+function Cabecalho_simples() {
+  return (
+    <>
+        <header class="cabecalho-simples">
+            <i class="fa-solid fa-arrow-left"></i>
+            <img class="logo_cabecalho-simples" src="imagens/cabecalho/parte01/logo.svg" alt=""/>
+        </header>
+    </>
+  )
+}
+
+export default Cabecalho_simples
