78548acadf7b6999142b3a3c734ef8216203e143
Mariana Gonçalves <mariana.goncalves@maristavirtual.org.br>
2023-03-05 21:53:47 -0300

    D

--- projetofreshfoodreact/src/App.js
+++ projetofreshfoodreact/src/App.js
@@ -1,14 +1,13 @@
 import { useState } from 'react';
 import './App.css';
-
-import Pagina_inicio from './componentes/paginas/Pagina_inicio';
+import {BrowserRouter} from 'react-router-dom'
+import Rota from '../src/Rota'
 
 function App() {
 
   return (
     <div className="App">
-      <Pagina_inicio/>
-    
+      <Rota/>
     </div>
   )
 }
