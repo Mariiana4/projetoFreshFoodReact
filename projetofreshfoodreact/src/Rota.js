9376381d98ddd0d74d659da51d060bf2885b0715
Mariana Gonçalves <mariana.goncalves@maristavirtual.org.br>
2023-03-05 21:52:09 -0300

    D

---
+++ projetofreshfoodreact/src/Rota.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Routes, Route } from 'react-router-dom'
+import Pagina_inicio from './componentes/paginas/Pagina_inicio'
+import Pagina_login from './componentes/paginas/Pagina_login'
+import Pagina_cadastro from './componentes/paginas/Pagina_cadastro'
+
+function RotaLogin() {
+  return (
+      <Routes>
+        <Route path="/" element={<Pagina_inicio/>}/>
+        <Route path="/login" element={<Pagina_login/>}/>
+        <Route path="/cadastro" element={<Pagina_cadastro/>}/>
+    </Routes>
+  )
+}
+
+export default RotaLogin
