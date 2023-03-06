e56d5db6b94a3db66be3d6f5fbbc1b2b3f75145f
Mariana Gonçalves <mariana.goncalves@maristavirtual.org.br>
2023-03-05 21:59:11 -0300

    Login

--- projetofreshfoodreact/src/componentes/cabecalho/cabecalho_simples/Cabecalho_simples.js
+++ projetofreshfoodreact/src/componentes/cabecalho/cabecalho_simples/Cabecalho_simples.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Cabecalho_simples.css'
 
 function Cabecalho_simples() {
   return (
     <>
         <header className="cabecalho-simples">
-            <i className="fa-solid fa-arrow-left"></i>
+            <Link to="/"><i className="fa-solid fa-arrow-left"></i></Link>
             <img className="logo_cabecalho-simples" src="imagens/cabecalho/parte01/logo.svg" alt=""/>
         </header>
     </>
--- projetofreshfoodreact/src/componentes/cabecalho/parte01/CabecalhoParte01.js
+++ projetofreshfoodreact/src/componentes/cabecalho/parte01/CabecalhoParte01.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './CabecalhoParte01.css'
 
 function cabecalhoParte01() {
@@ -27,7 +28,7 @@ function cabecalhoParte01() {
         
                 <i class="navIcones fa-solid fa-basket-shopping" id='cestaIcone'></i>
         
-                <i class="navIcones fa-solid fa-circle-user" id='userIcone'></i>
+                <Link to="/login"><i class="navIcones fa-solid fa-circle-user" id='userIcone'></i></Link>
     
             </nav>
         </div>
