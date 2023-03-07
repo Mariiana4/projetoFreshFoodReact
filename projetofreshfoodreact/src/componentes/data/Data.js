40ca48261b5b8cae71c18031f70aeb6708d818fc
g-sillva <114768002+isa24dora@users.noreply.github.com>
2023-03-07 17:10:39 -0300

    Atualização das informações do card

--- projetofreshfoodreact/src/componentes/data/Data.js
+++ projetofreshfoodreact/src/componentes/data/Data.js
@@ -2,98 +2,109 @@ export const Receitas = [
     {
         "imagemCardReceitas": "/imagens/cardReceitas/receita.jpg",
         "tagCardReceitas": "Vegetariano",
-        "nomeCardReceitas": "Salada de atum",
+        "nomeCardReceitas": "Beringela recheada com quinoa",
         "tempoCardReceitas": "40 min",
         "dificuldadeCardReceitas": "Médio",
-        "porcaoCardReceitas": "5",
-        "precoCardReceitas": "10,50",
+        "porcaoCardReceitas": "4",
+        "precoCardReceitas": "22,60",
         "itensCardReceitas": [
-            "1 maço de alface americana",
-            "1 maço de alface crespa",
-            "1 xícara de chá de tomate-cereja",
-            "1 xícara de chá de palmito pupunha",
-            "1 manga picada",
-            "Sal",
-            "Azeite",
-            "Orégano",
-            "3 batatas",
-            "4 tomates"
+            "2 berinjelas",
+            "sal",
+            "pimenta",
+            "1 dente de alho picado",
+            "400 g de quinoa cozida",
+            "1 cebola picada",
+            "250 g de tomate cereja",
+            "2 colheres de sopa de hortelã picada",
+            "2 colheres de sopa de coentro picados",
+            "1 lata de grão",
+            "1 colher de sopa de molho tahini"
+        ]
+    },
+    {
+        "imagemCardReceitas": "/imagens/cardReceitas/receita02.jpg",
+        "tagCardReceitas": "Vegetariano",
+        "nomeCardReceitas": "Rolo de lentilhas com compota de frutos vermelhos",
+        "tempoCardReceitas": "20 min",
+        "dificuldadeCardReceitas": "Fácil",
+        "porcaoCardReceitas": "8",
+        "precoCardReceitas": "56,90",
+        "itensCardReceitas": [
+            "0,5 cebola picada",
+            "1 dente de alho",
+            "1 alho-poró",
+            "1 talo de aipo",
+            "1 cenoura em cubos",
+            "50 g de cogumelos",
+            "1 colher de sopa de uvas passa",
+            "210 g de lentilhas cozidas",
+            "50 g de pão ralado",
+            "1 colherde sopa de salsa picada",
+            "sal",
+            "pimenta do reino moída",
+            "50 g de compota de frutas vermelhas",
+            "groselha, a gosto",
+            "alecrim, a gosto"
+        ]
+    },
+    {
+        "imagemCardReceitas": "/imagens/cardReceitas/receita.jpg",
+        "tagCardReceitas": "Vegano",
+        "nomeCardReceitas": "Bolo de laranja com alecrim vegano",
+        "tempoCardReceitas": "120 minutos",
+        "dificuldadeCardReceitas": "médio",   
+        "porcaoCardReceitas": "15",
+        "precoCardReceitas": "40,00",
+        "itensCardReceitas": [
+            "120 g de mix de farinhas sem glúten",
+            "100 g de farelo de aveia",
+            "150 g de açucar demarara",
+            "70 ml de suco de laranja",
+            "180 ml de água",
+            "100 ml de azeite de oliva",
+            "2 ramos de alecrim sem o talo",
+            "10 g de fermento químico",
+            "1 colher de sopa de vinagre de maçã",
+            "1 pitada de sal",
+            "açucar a gosto",
+            "ramos de alecrim para finalizar"
+        ]
+    },
+    {
+        "imagemCardReceitas": "/imagens/cardReceitas/receita.jpg",
+        "tagCardReceitas": "Vegano",
+        "nomeCardReceitas": "French toast vegano",
+        "tempoCardReceitas": "30 minutos",
+        "dificuldadeCardReceitas": "fácil",
+        "porcaoCardReceitas": "8",
+        "precoCardReceitas": "44,90",
+        "itensCardReceitas": [
+            "1 copo de leite vegetal",
+            "1/2 colher de chá de canela em pó",
+            "2 colheres de chá de farinha de linhaça dourada",
+            "1 colher de sopa de açucar demerara",
+            "8 fatias de pão com cerca de 1 cm de espessura",
+            "óleo de coco",
+            "maple syrup a gosto",
+            "banana picada a gosto"
         ]
     },
     {
         "imagemCardReceitas": "/imagens/cardReceitas/receita02.jpg",
         "tagCardReceitas": "Vegano",
-        "nomeCardReceitas": "Torta de morango",
-        "tempoCardReceitas": "20 min",
+        "nomeCardReceitas": "pão de queijo vegano",
+        "tempoCardReceitas": "100 min",
         "dificuldadeCardReceitas": "Fácil",
-        "porcaoCardReceitas": "8",
-        "precoCardReceitas": "1050",
+        "porcaoCardReceitas": "20",
+        "precoCardReceitas": "32,90",
         "itensCardReceitas": [
-            "1 maço de alface americana",
-            "1 maço de alface crespa",
-            "1 xícara de chá de tomate-cereja",
-            "1 xícara de chá de palmito pupunha",
-            "1 manga picada",
-            "Sal",
-            "azeite",
-            "orégano"
-        ]
-    },
-    {
-        "imagemCardReceitas": "/imagens/cardReceitas/receita.jpg",
-        "tagCardReceitas": "Zero lactose",
-        "nomeCardReceitas": "Pão de aveia",
-        "tempoCardReceitas": "1 hora",
-        "dificuldadeCardReceitas": "difícil",   
-        "porcaoCardReceitas": "15",
-        "precoCardReceitas": "1050",
-        "itensCardReceitas": [
-            "1 maço de alface americana",
-            "1 maço de alface crespa",
-            "1 xícara de chá de tomate-cereja",
-            "1 xícara de chá de palmito pupunha",
-            "1 manga picada",
-            "Sal",
-            "azeite",
-            "orégano"
-        ]
-    },
-    {
-        "imagemCardReceitas": "/imagens/cardReceitas/receita.jpg",
-        "tagCardReceitas": "Zero lactose",
-        "nomeCardReceitas": "Pão de aveia",
-        "tempoCardReceitas": "1 hora",
-        "dificuldadeCardReceitas": "difícil",
-        "porcaoCardReceitas": "15",
-        "precoCardReceitas": "1050",
-        "itensCardReceitas": [
-            "1 maço de alface americana",
-            "1 maço de alface crespa",
-            "1 xícara de chá de tomate-cereja",
-            "1 xícara de chá de palmito pupunha",
-            "1 manga picada",
-            "Sal",
-            "azeite",
-            "orégano"
-        ]
-    },
-    {
-        "imagemCardReceitas": "/imagens/cardReceitas/receita02.jpg",
-        "tagCardReceitas": "Vegano",
-        "nomeCardReceitas": "Torta de morango",
-        "tempoCardReceitas": "20 min",
-        "dificuldadeCardReceitas": "Fácil",
-        "porcaoCardReceitas": "8",
-        "precoCardReceitas": "1050",
-        "itensCardReceitas": [
-            "1 maço de alface americana",
-            "1 maço de alface crespa",
-            "1 xícara de chá de tomate-cereja",
-            "1 xícara de chá de palmito pupunha",
-            "1 manga picada",
-            "Sal",
-            "azeite",
-            "orégano"
+            "2 xícaras de polvilho doce",
+            "1 e 1/2 xícaras de batata baroa bem cozida e amassada",
+            "1/2 xícaras de polvilho azedo",
+            "1/3 de xícara de óleo vegetal",
+            "1/4 de xícara de água morna",
+            "Sal a gosto"
+            
         ]
     }
 ]
