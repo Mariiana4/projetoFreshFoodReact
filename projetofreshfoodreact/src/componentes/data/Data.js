bef869f05adecea08438ebadd5d8a240975cdef2
g-sillva <114768002+isa24dora@users.noreply.github.com>
2023-03-08 15:28:32 -0300

    atualização de todas as receitas

--- projetofreshfoodreact/src/componentes/data/Data.js
+++ projetofreshfoodreact/src/componentes/data/Data.js
@@ -106,5 +106,172 @@ export const Receitas = [
             "Sal a gosto"
             
         ]
+    },
+    {
+        "imagemCardReceitas": "/imagens/cardReceitas/receita02.jpg",
+        "tagCardReceitas": "Restrições alimentares",
+        "nomeCardReceitas": "Panqueca sem glúten",
+        "tempoCardReceitas": "30 min",
+        "dificuldadeCardReceitas": "Fácil",
+        "porcaoCardReceitas": "4",
+        "precoCardReceitas": "29,90",
+        "itensCardReceitas": [
+            "1 xícara de farinha de aveia (sem glúten)",
+            "1 inhame médio",
+            "1 xícara de água",
+            "1/4 xícara de polvilho doce",
+            "1 colher de sopa de óleo vegetal",
+            "1/4 colher de chá de sal"
+            
+        ]
+    },
+    {
+        "imagemCardReceitas": "/imagens/cardReceitas/receita02.jpg",
+        "tagCardReceitas": "Restrições alimentares",
+        "nomeCardReceitas": "Creme de mandioquinha com queijo vegetal (sem Lactose)",
+        "tempoCardReceitas": "45 min",
+        "dificuldadeCardReceitas": "Fácil",
+        "porcaoCardReceitas": "6",
+        "precoCardReceitas": "26,90",
+        "itensCardReceitas": [
+            "400 gr de mandioquinha ",
+            "1 cebola",
+            "1 colher de sobremesa de azeite de oliva",
+            "1 litro de água",
+            "cebolinha a gosto",
+            "sal e pimenta a gosto"
+            
+        ]
+    },
+    {
+        "imagemCardReceitas": "/imagens/cardReceitas/receita02.jpg",
+        "tagCardReceitas": "Restrições alimentares",
+        "nomeCardReceitas": "Chandelle (sem Lactose)",
+        "tempoCardReceitas": "20 min",
+        "dificuldadeCardReceitas": "Fácil",
+        "porcaoCardReceitas": "4",
+        "precoCardReceitas": "22,90",
+        "itensCardReceitas": [
+            "2 bananas congeladas",
+            "1 colher de sopa de cacau em pó",
+            "4 pedras de gelo",
+            "150 ml de leite vegetal"
+        ]
+    },
+    {
+        "imagemCardReceitas": "/imagens/cardReceitas/receita02.jpg",
+        "tagCardReceitas": "Restrições alimentares",
+        "nomeCardReceitas": "Pão de queijo (sem glúten)",
+        "tempoCardReceitas": "40 min",
+        "dificuldadeCardReceitas": "médio",
+        "porcaoCardReceitas": "6",
+        "precoCardReceitas": "38,90",
+        "itensCardReceitas": [
+            "2 xícaras de polvilho doce",
+            "1/2 xícaras de polvilho azedo",
+            "11/2 xícaras de batata baroa (ou mandioquinha) bem cozida e amassada no estilo purê",
+            "1/3 de xícaras de óleo vegetal",
+            "1/4 de xícaras de água morna",
+            "sal a gosto"
+        ]
+    },
+    {
+        "imagemCardReceitas": "/imagens/cardReceitas/receita02.jpg",
+        "tagCardReceitas": "Sobremesas",
+        "nomeCardReceitas": "Mousse Clássica de Chocolate",
+        "tempoCardReceitas": "200 min",
+        "dificuldadeCardReceitas": "fácil",
+        "porcaoCardReceitas": "6",
+        "precoCardReceitas": "22,50",
+        "itensCardReceitas": [
+            "1 lata de creme de leite",
+            "200 g de chocolate meio amargo picado",
+            "3 claras",
+            "3 colheres de sopa de açucar"
+        ]
+    },
+    {
+        "imagemCardReceitas": "/imagens/cardReceitas/receita02.jpg",
+        "tagCardReceitas": "Sobremesas",
+        "nomeCardReceitas": "Surpresa de morangos com merengue",
+        "tempoCardReceitas": "55 min",
+        "dificuldadeCardReceitas": "fácil",
+        "porcaoCardReceitas": "8",
+        "precoCardReceitas": "24,90",
+        "itensCardReceitas": [
+            "1 caixa de leite consensado",
+            "2 caixas de leite em pó",
+            "3 gemas",
+            "1 colher de sopa de amido de milho",
+            "meia xícara de açucar",
+            "1 caixinha de morangos"
+        ]
+    },
+    {
+        "imagemCardReceitas": "/imagens/cardReceitas/receita02.jpg",
+        "tagCardReceitas": "Sobremesas",
+        "nomeCardReceitas": "Bombom de nozes",
+        "tempoCardReceitas": "35 min",
+        "dificuldadeCardReceitas": "médio",
+        "porcaoCardReceitas": "30",
+        "precoCardReceitas": "24,90",
+        "itensCardReceitas": [
+            "1 caixa de leite consensado",
+            "1 colher de sopa de manteiga",
+            "1 xícara de de nozes picadas",
+            "300 g de cobertura de chocolate ao leite"   
+        ]
+    },
+    {
+        "imagemCardReceitas": "/imagens/cardReceitas/receita02.jpg",
+        "tagCardReceitas": "Carnes",
+        "nomeCardReceitas": "Carne assada com batata",
+        "tempoCardReceitas": "120 min",
+        "dificuldadeCardReceitas": "médio",
+        "porcaoCardReceitas": "3",
+        "precoCardReceitas": "68,90",
+        "itensCardReceitas": [
+            "4 batatas médias",
+            "600 g de carne",
+            "1 cebola picada",
+            "1 colher de sopa de sal",
+            "1 pitada de pimenta-do-reino",
+            "1 colher de chá de páprica doce",
+            "2 colheres de sopa de azeite",
+            "200 g de mussarela ralada",
+            "orégano a gosto" 
+        ]
+    },
+    {
+        "imagemCardReceitas": "/imagens/cardReceitas/receita02.jpg",
+        "tagCardReceitas": "Carnes",
+        "nomeCardReceitas": "Fraldinha assada com manteiga e alho",
+        "tempoCardReceitas": "35 min",
+        "dificuldadeCardReceitas": "médio",
+        "porcaoCardReceitas": "5",
+        "precoCardReceitas": "64,90",
+        "itensCardReceitas": [
+            "850 g de fraldinha sem a gordura",
+            "sal a gosto",
+            "pimenta a gosto",
+            "2 dentes de alho",
+            "2 colheres de sopa de manteiga" 
+        ]
+    },
+    {
+        "imagemCardReceitas": "/imagens/cardReceitas/receita02.jpg",
+        "tagCardReceitas": "Carnes",
+        "nomeCardReceitas": "Fraldinha assada na mostarda",
+        "tempoCardReceitas": "90 min",
+        "dificuldadeCardReceitas": "médio",
+        "porcaoCardReceitas": "4",
+        "precoCardReceitas": "64,90",
+        "itensCardReceitas": [
+            "2 colheres de sopa de manteiga (amolecida)",
+            "2 dentes de alho",
+            "2 colheres de sopa de mostarda",
+            "500 g a 1,2 kg de peça de carne (carnes macias)",
+            "sal e pimenta-do-reino a gosto" 
+        ]
     }
 ]
