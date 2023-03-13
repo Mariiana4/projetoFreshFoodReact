8658c7610387abed55ed4f936afeeda3afec2423
Mariana Gonçalves <mariana.goncalves@maristavirtual.org.br>
2023-03-13 14:32:25 -0300

    Formulario de cadastro

--- projetofreshfoodreact/src/componentes/formularioEntrar/Formulario_entrar.js
+++ projetofreshfoodreact/src/componentes/formularioEntrar/Formulario_entrar.js
@@ -1,27 +1,74 @@
+import { useState, UseState } from 'react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Formulario_entrar.css'
 
+
 function Formulario_entrar() {
+
+    const [formData, setFormData] = useState({
+        emailEntrar: '',
+        senhaEntrar: '',
+    })
+
+    const handleFormEdit = (event, emailEntrar) => {
+        setFormData({
+            ...formData,
+            [emailEntrar]: event.target.value   
+        })
+    }
+
+    const handleForm = async (event) => {
+        try{
+            event.preventDefault()
+            const response = await fetch(``, {
+                method: '',
+                body: JSON.stringify(formData )
+            })
+            const json = await response.json()
+            console.log(json)
+        } catch (err) {
+
+        }
+    }
+
   return (
     <>
     <main>
         <div className="container_formCadastro">
             <h2 className="titulo-cadastro_formCadastro">Faça seu login</h2>
 
-            <form className="container_input-label_formCadastro">
+            <form className="container_input-label_formCadastro" >
                 <div className="input-label_formCadastro">
                     <label className="label_formCadastro" for="email">E-mail (Campo obrigatório)</label>
-                    <input className="input_fomrCadastro" type="email" name="email" id="email" placeholder="Preencha com seu e-mail" required/>
+                    <input 
+                        className="input_formCadastro" 
+                        type="email" 
+                        name="email" 
+                        id="email" 
+                        placeholder="Preencha com seu e-mail" 
+                        required
+                        value={formData.emailEntrar}
+                        onChange={(e) => {handleFormEdit(e, 'emailEntrar')}}
+                    />
                 </div>
 
                 <div className="input-label_formCadastro">
                     <label className="label_formCadastro" for="senha">Senha (Campo obrigatório)</label>
-                    <input className="input_fomrCadastro" type="password" name="senha" id="senha" placeholder="Insira a sua senha" required/>
+                    <input 
+                        className="input_formCadastro" 
+                        type="password" 
+                        name="senha" 
+                        id="senha" 
+                        placeholder="Insira a sua senha" 
+                        required
+                        value={formData.senhaEntrar}
+                        onChange={(event) => {handleFormEdit(event, 'senhaEntrar')}}
+                    />
                 </div>
 
                 <div className="container-botao_formLogin">
-                    <button className="botao-finalizar_formLogin">Entrar</button>
+                    <button type='submit' className="botao-finalizar_formLogin">Entrar</button>
                 </div>
 
                 <section className="container_esqueceuSenha">
