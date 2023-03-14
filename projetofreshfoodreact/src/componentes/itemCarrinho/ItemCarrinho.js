fdfdd0d2cd8a058aef4b67e0dcb1e0ed9ffcc29a
Mariana Gonçalves <mariana.goncalves@maristavirtual.org.br>
2023-03-14 10:16:13 -0300

    Item adicionados ao carrinho

--- projetofreshfoodreact/src/componentes/itemCarrinho/ItemCarrinho.js
+++ projetofreshfoodreact/src/componentes/itemCarrinho/ItemCarrinho.js
@@ -10,24 +10,82 @@ function ItemCarrinho() {
         <div className="container_itemIndividual_cestoDeCompras">
             <div className="container_informacoes_cestoDeCompras">
                 <div className="item_cestoDeCompras">
-                    <img className="imagem_item_cestoDeCompras" src="imagens/cardReceitas/receita.jpg" alt=""/>
+                    <img className="imagem_item_cestoDeCompras" src="/imagens/cardReceitas/receitas/imagens_receita/imagem_receita_bolo-laranja.jpg" alt=""/>
         
                     <div className="nome_ingredientes_tag_remover_cestoDeCompras">
-                        <h2 className="nome_cestoDeCompras">Salada de batata </h2>
+                        <h2 className="nome_cestoDeCompras">Bolo de laranja com alecrim vegano</h2>
         
                         <div className="container_tag_remover_cestoDeCompras">
-                            <p className="tag_cestoDeCompras">Sem glúten</p>
+                            <p className="tag_cestoDeCompras">Vegano</p>
                             <p className="remover_cestoDeCompras">Remover</p>
                         </div>
                     </div>
                 </div>
         
                 <div className="preco_quantidade_cestoDeCompras">
-                    <p className="preco_cestoDeCompras">R$ 20,00</p>
+                    <p className="preco_cestoDeCompras">R$ 40,00</p>
                     
                     <div className="container_quantidade_cestoDeCompras">
                         <p className="remover_quantidade_cestoDeCompras">-</p>
-                        <p className="quantidade_cestoDeCompras">3</p>
+                        <p className="quantidade_cestoDeCompras">1</p>
+                        <p className="adicionar_quantidade_cestoDeCompras">+</p>
+                    </div>
+                </div>
+    
+            </div>
+            <hr className="linha-divisao_cestoDeCompras"/>
+        </div>
+
+        <div className="container_itemIndividual_cestoDeCompras">
+            <div className="container_informacoes_cestoDeCompras">
+                <div className="item_cestoDeCompras">
+                    <img className="imagem_item_cestoDeCompras" src="/imagens/cardReceitas/receitas/imagens_receita/imagem_receita_pao-queijo.jpg" alt=""/>
+        
+                    <div className="nome_ingredientes_tag_remover_cestoDeCompras">
+                        <h2 className="nome_cestoDeCompras">Pão de queijo vegano</h2>
+        
+                        <div className="container_tag_remover_cestoDeCompras">
+                            <p className="tag_cestoDeCompras">Vegano</p>
+                            <p className="remover_cestoDeCompras">Remover</p>
+                        </div>
+                    </div>
+                </div>
+        
+                <div className="preco_quantidade_cestoDeCompras">
+                    <p className="preco_cestoDeCompras">R$ 32,90</p>
+                    
+                    <div className="container_quantidade_cestoDeCompras">
+                        <p className="remover_quantidade_cestoDeCompras">-</p>
+                        <p className="quantidade_cestoDeCompras">2</p>
+                        <p className="adicionar_quantidade_cestoDeCompras">+</p>
+                    </div>
+                </div>
+    
+            </div>
+            <hr className="linha-divisao_cestoDeCompras"/>
+        </div>
+
+        <div className="container_itemIndividual_cestoDeCompras">
+            <div className="container_informacoes_cestoDeCompras">
+                <div className="item_cestoDeCompras">
+                    <img className="imagem_item_cestoDeCompras" src="/imagens/cardReceitas/receitas/imagens_receita/imagem_receita_morango-suspiro.jpg" alt=""/>
+        
+                    <div className="nome_ingredientes_tag_remover_cestoDeCompras">
+                        <h2 className="nome_cestoDeCompras">Surpresa de morangos com merengue</h2>
+        
+                        <div className="container_tag_remover_cestoDeCompras">
+                            <p className="tag_cestoDeCompras">Sobremesa</p>
+                            <p className="remover_cestoDeCompras">Remover</p>
+                        </div>
+                    </div>
+                </div>
+        
+                <div className="preco_quantidade_cestoDeCompras">
+                    <p className="preco_cestoDeCompras">R$ 24,90</p>
+                    
+                    <div className="container_quantidade_cestoDeCompras">
+                        <p className="remover_quantidade_cestoDeCompras">-</p>
+                        <p className="quantidade_cestoDeCompras">1</p>
                         <p className="adicionar_quantidade_cestoDeCompras">+</p>
                     </div>
                 </div>
