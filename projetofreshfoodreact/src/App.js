b89e704cb83085ea7bbf4d4065e33b91f84f4b2d
Mariana Gonçalves <mariana.goncalves@maristavirtual.org.br>
2023-03-14 10:05:24 -0300

    .

--- projetofreshfoodreact/src/App.js
+++ projetofreshfoodreact/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import {BrowserRouter} from 'react-router-dom'
 import Rota from '../src/Rota'
 
-import ItemCarrinho from './componentes/itemCarrinho/ItemCarrinho';
 
 function App() {
 
